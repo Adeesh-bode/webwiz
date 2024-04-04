764b63baf39dbeb3283c1fa73fe0bb4eac32a745
Adesh Bode <bodead@rknec.edu>
2024-04-04 05:36:39 -0700

    2

--- app/utils/firebaseconfig.js
+++ app/utils/firebaseconfig.js
@@ -4,14 +4,24 @@
 import { initializeApp } from "firebase/app";
 
 // Firebase configuration
-const firebaseConfig = {  
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+// const firebaseConfig = {  
+//     apiKey: process.env.NEXT_PUBLIC_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+//     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+//     appId: process.env.NEXT_PUBLIC_APP_ID,
+//     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+// };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCR411w1kDw1Xdzm115_Ic85cJcJiXxduY",
+    authDomain: "webwiz-c5e1c.firebaseapp.com",
+    projectId: "webwiz-c5e1c",
+    storageBucket: "webwiz-c5e1c.appspot.com",
+    messagingSenderId: "142095787391",
+    appId: "1:142095787391:web:77789d9927edc04219a64a",
+    measurementId: "G-2SZSF56289"
 };
 
 // Initialize Firebase
