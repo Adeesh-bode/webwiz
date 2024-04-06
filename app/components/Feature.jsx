39cf13290a399ea7d00578f3392b9a451e10636b
Adesh Bode <bodead@rknec.edu>
2024-04-05 20:18:10 -0700

    3

--- app/components/Feature.jsx
+++ app/components/Feature.jsx
@@ -11,7 +11,7 @@ const Feature = ({ index, feature }) => {
          initial={{opacity:0, x: from }}
          animate={{opacity:1 , x:'0', transition: { duration: 2} }}
         >
-            <img src={feature.path} alt={feature.title} className="w-40 md:w-64 h-auto max-w-xs mx-auto my-4 md:my-0" />
+            <img src={feature.path} alt={feature.title} className="w-40 md:w-64 h-auto max-w-xs mx-auto" />
             <div className='flex flex-col gap-8 items-center md:items-start text-center md:text-left'>
                 <h1 className='text-3xl md:text-5xl bg-gradient-to-b from-gray-900 to-white bg-clip-text text-transparent'>{feature.title}</h1>
                 <p className='w-full md:w-[500px] font-sans font-extralight text-sm md:text-xl'>{feature.description}</p>
