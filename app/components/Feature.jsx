ac96db0a98ce8c6ac22a47221e47ca269c2f9b17
Adesh Bode <bodead@rknec.edu>
2024-04-05 20:40:50 -0700

    changes

--- app/components/Feature.jsx
+++ app/components/Feature.jsx
@@ -7,11 +7,11 @@ const Feature = ({ index, feature }) => {
     const from = (index %2==0) ? '-100vw' : '100vw';
     return (
         <motion.div 
-         className={`min-h-screen flex flex-col md:flex-row ${index % 2 !== 0 && 'md:flex-row-reverse'} justify-between items-center px-4 md:px-56 w-full`}
+         className={`min-h-screen flex flex-col md:flex-row ${index % 2 !== 0 && 'md:flex-row-reverse'}  justify-center md:justify-between items-center px-4 md:px-56 w-full`}
          initial={{opacity:0, x: from }}
          animate={{opacity:1 , x:'0', transition: { duration: 2} }}
         >
-            <img src={feature.path} alt={feature.title} className="w-40 md:w-64 h-auto max-w-xs mx-auto" />
+            <img src={feature.path} alt={feature.title} className="w-40 md:w-64 h-auto max-w-xs mx-auto my-4 md:my-0" />
             <div className='flex flex-col gap-8 items-center md:items-start text-center md:text-left'>
                 <h1 className='text-3xl md:text-5xl bg-gradient-to-b from-gray-900 to-white bg-clip-text text-transparent'>{feature.title}</h1>
                 <p className='w-full md:w-[500px] font-sans font-extralight text-sm md:text-xl'>{feature.description}</p>
